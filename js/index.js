801f488c000eb9c1cfb445ece5bbcfa7202c9831
Pedro <pedrojimenezhontanilla@gmail.com>
2019-05-09 11:05:49 +0200

    added breed select input  using axios to get the breedlist

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 new Vue({
     el: '#app',
     data: { 
-        breedList: ['breed 01' , 'breed 02']
-    }
+        breedList: [],
+        errMessage: ''
+    },
+    mounted() {
+        this.getBreedList()
+    },
+    methods: {
+        getBreedList(){
+            const API = 'https://dog.ceo/api/breeds/list/all'
+            axios.get(API)
+          .then( (response) => {
+              const res = response.data.message
+                console.log(res)
+              this.breedList = Object.keys(res),
+              this.breesListSearch = this.breedList
+          })
+          .catch(function (error) {
+            this.errMessage = 'We can´t brind an amaizing dog pictures'
+          })
+
+        }
+    },
 })
